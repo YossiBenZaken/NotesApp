98cda7a87cfb907c1ddf2f96309803b99d8b34ab
YossiBenZaken <yossibz@gmail.com>
2022-08-19 16:56:20 +0300

    Initial Commit

---
+++ Shared/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Shared
+//
+//  Created by Yosef Ben Zaken on 19/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
